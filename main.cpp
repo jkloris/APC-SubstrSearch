92f7db2abc114faf1ab1ad468884ab227c709645
jkloris <kapusta.jergus@gmail.com>
2022-10-24 15:59:47 +0200

    upravenie hashu

--- main.cpp
+++ main.cpp
@@ -13,10 +13,11 @@ struct args
 
 uint32_t hashString(std::string str);
 std::optional<args> getArgs(int argc, char* argv[]);
-
+uint32_t hashModifyString(std::string str, uint32_t hash, char first);
 
 int main(int argc, char* argv[])
 {
+    
     std::optional<args> arguments = getArgs(argc, argv);
     if (!arguments) 
         return EXIT_FAILURE;
@@ -28,23 +29,26 @@ int main(int argc, char* argv[])
     unsigned int i = 0, absi = 0;
     for (; i < arguments.value().X.size();i++) {
         arguments.value().file.get(c);
+
+        if (int(c) < 0 || int(c) > 127) {
+            std::cout << int(c) << " " << c << '\n';
+            return EXIT_FAILURE;
+        }
+
         substring.push_back(c);
         
     }
     i = 0;
     const uint32_t xHash = hashString(arguments.value().X);
-    uint32_t subHash;
+    uint32_t subHash = hashString(substring);
 
  
     int posDist = -1, absPosSub = -INT_MAX, rowDist = 0;
 
     while (arguments.value().file) {
-        //std::cout << absi << " " << i << " " << row << " " << posDist << " " << rowDist<<"\n";
-       // std::cout << absi << " " << i << " " << row << "\n";
+
         //Rain-Karp search algorithm
-        subHash = hashString(substring);
         if (xHash == subHash && arguments.value().X == substring) {
-            //std::cout << row << " " << i << "\n";
 
             if (absi - absPosSub  <= arguments.value().N) {
 
@@ -56,7 +60,7 @@ int main(int argc, char* argv[])
                 std::cout << row << " " << i  << "\n";
             }
             else {
-                //std::cout << absPosSub << '\n';
+
                 absPosSub = absi;
                 posDist = i;
                 rowDist = row;
@@ -68,6 +72,8 @@ int main(int argc, char* argv[])
 
         // get new char
         arguments.value().file.get(c);
+        if (int(c) < 0 || int(c) > 127)
+            return EXIT_FAILURE;
 
         //counting
         i++;
@@ -78,8 +84,10 @@ int main(int argc, char* argv[])
         }
 
         // shift substring by one char
+        char first = substring[0];
         substring.push_back(c);
         substring.erase(substring.begin());
+        subHash = hashModifyString(substring, subHash, first);
 
     }
 
@@ -99,6 +107,8 @@ std::optional<args> getArgs(int argc, char* argv[]) {
         return {};
 
     arguments.X = argv[2];
+    if (arguments.X.size() >= 256)
+        return {};
 
     uint32_t a = std::stoi(argv[3]);
     arguments.N = a;
@@ -114,13 +124,21 @@ uint32_t hashString(std::string str)
     for (unsigned int i = 0; i < str.size(); i++)
     {
         hash += str[i];
-        hash += (hash << 10);
-        hash ^= (hash >> 6);
+        //hash += (hash << 4);
+        //hash ^= (hash >> 6);
     }
 
-    hash += (hash << 3);
-    hash ^= (hash >> 11);
-    hash += (hash << 15);
+    //hash += (hash << 3);
+    //hash ^= (hash >> 11);
+    //hash += (hash << 15);
+
+    return hash;
+}
+
+uint32_t hashModifyString(std::string str, uint32_t hash, char first)
+{
+        hash -= first;
+        hash += str[str.size() - 1];
 
     return hash;
 }
