73206d8943f6b9c4fa4fa76188b7cd25eb7a4a67
jkloris <kapusta.jergus@gmail.com>
2022-10-26 17:05:31 +0200

    optimalizacia

--- main.cpp
+++ main.cpp
@@ -8,49 +8,54 @@ struct args
 {
     std::ifstream file;
     std::string X;
-    uint32_t N;
+    uint32_t N{};
 };
+
+
 
 uint32_t hashString(std::string str);
 std::optional<args> getArgs(int argc, char* argv[]);
-uint32_t hashModifyString(std::string str, uint32_t hash, char first);
+uint32_t hashModifyString(std::string str, uint32_t hash, char first); //obsolite
 
 int main(int argc, char* argv[])
 {
-    
+
     std::optional<args> arguments = getArgs(argc, argv);
-    if (!arguments) 
+    if (!arguments)
         return EXIT_FAILURE;
-    
+
+    auto argumen = *std::move(arguments);
+
+
     //read first size of N chars
-    char c;
-    unsigned row = 0;
-    std::string substring = "";
+    char c = {}, first;
+    unsigned int row = 0, buffi = 0;
+    std::string substring(argumen.X.size(), '0');
+
     unsigned int i = 0, absi = 0;
-    for (; i < arguments.value().X.size();i++) {
-        arguments.value().file.get(c);
+    for (; i < argumen.X.size();i++) {
+        argumen.file.get(c);
 
         if (int(c) < 0 || int(c) > 127) {
             std::cout << int(c) << " " << c << '\n';
             return EXIT_FAILURE;
         }
 
-        substring.push_back(c);
-        
+        substring[i] = c;
     }
-    i = 0;
-    const uint32_t xHash = hashString(arguments.value().X);
-    uint32_t subHash = hashString(substring);
 
+    i = 0;
+    const uint32_t xHash = hashString(argumen.X);
+    uint32_t subHash = hashString(substring);
  
     int posDist = -1, absPosSub = -INT_MAX, rowDist = 0;
 
-    while (arguments.value().file) {
+    while (argumen.file) {
 
         //Rain-Karp search algorithm
-        if (xHash == subHash && arguments.value().X == substring) {
+        if (xHash == subHash && argumen.X == substring) {
 
-            if (absi - absPosSub  <= arguments.value().N) {
+            if (absi - absPosSub  <= argumen.N) {
 
                 if (posDist >= 0) {
                     std::cout << rowDist << " " << posDist  << "\n";
@@ -65,13 +70,12 @@ int main(int argc, char* argv[])
                 posDist = i;
                 rowDist = row;
 
-
             }
             
         }
 
         // get new char
-        arguments.value().file.get(c);
+        argumen.file.get(c);
         if (int(c) < 0 || int(c) > 127)
             return EXIT_FAILURE;
 
@@ -84,10 +88,15 @@ int main(int argc, char* argv[])
         }
 
         // shift substring by one char
-        char first = substring[0];
-        substring.push_back(c);
-        substring.erase(substring.begin());
-        subHash = hashModifyString(substring, subHash, first);
+         first = substring[0];
+        
+        for (buffi = 0; buffi < substring.size() - 1; buffi++) {
+            substring[buffi] = substring[buffi + 1];
+        }
+        substring[buffi] = c;
+
+        subHash -= first;
+        subHash += substring[substring.size() - 1];
 
     }
 
@@ -95,7 +104,7 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-//TODO checking
+
 std::optional<args> getArgs(int argc, char* argv[]) {
     if (argc != 4)
         return {};
@@ -109,32 +118,36 @@ std::optional<args> getArgs(int argc, char* argv[]) {
     arguments.X = argv[2];
     if (arguments.X.size() >= 256)
         return {};
+    //arguments.X = "Abababaabaabdbaaacbbbcbcacaab\ncdaadadcdcabbdbddadcadddcacaacbaabaabddacbcaddccaccddbdcdcc\ncbbddaddacabadccdccbbacabdccdccdbdbadacccbaacddcddadaacdabbaaddabdcabbcbdadadb\nabddaccdccccdddabbacdabcacdbcdabcbbcbb\nbcabbbcbbadbaaabdcbbaddcdacdadacccdddbaaacdd"; //; TMP hardcoded 
 
-    uint32_t a = std::stoi(argv[3]);
+    int a;
+    try {
+        a = std::stoi(argv[3]);
+        if (a <= 0)
+            throw(std::runtime_error(argv[3]));
+    }
+    catch(std::exception& e) {
+        std::cout << e.what();
+        return {};
+    }
     arguments.N = a;
 
     return arguments;
 }
 
 
-// inspired by algorithm from https://stackoverflow.com/questions/114085/fast-string-hashing-algorithm-with-low-collision-rates-with-32-bit-integer
 uint32_t hashString(std::string str)
 {
     uint32_t hash = 0;
     for (unsigned int i = 0; i < str.size(); i++)
     {
         hash += str[i];
-        //hash += (hash << 4);
-        //hash ^= (hash >> 6);
     }
-
-    //hash += (hash << 3);
-    //hash ^= (hash >> 11);
-    //hash += (hash << 15);
 
     return hash;
 }
 
+//Obsolite
 uint32_t hashModifyString(std::string str, uint32_t hash, char first)
 {
         hash -= first;
