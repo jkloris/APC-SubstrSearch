53872f22a6d20fc9002d452c8758aa695f506f1f
jkloris <kapusta.jergus@gmail.com>
2022-10-28 11:33:17 +0200

    pridany poradny buffer, im speed

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
 
     //read first size of N chars
     char c = {}, first;
-    unsigned int row = 0, buffi = 0;
+    unsigned int row = 0, shifti = 0, buffi = 0;
     std::string substring(argumen.X.size(), '0');
 
     unsigned int i = 0, absi = 0;
@@ -46,61 +46,80 @@ int main(int argc, char* argv[])
     }
 
     i = 0;
-    const uint32_t xHash = hashString(argumen.X);
+    const uint32_t xHash = hashString(argumen.X); 
     uint32_t subHash = hashString(substring);
  
     int posDist = -1, absPosSub = -INT_MAX, rowDist = 0;
 
-    while (argumen.file) {
 
-        //Rain-Karp search algorithm
-        if (xHash == subHash && argumen.X == substring) {
+    char buffer[32768];
+    std::streamsize bufferSize = std::size(buffer);
+    size_t gc = 0;
 
-            if (absi - absPosSub  <= argumen.N) {
+    argumen.file.read(buffer, bufferSize);
+    gc = argumen.file.gcount();
+    
+    while (gc > 0) {
 
-                if (posDist >= 0) {
-                    std::cout << rowDist << " " << posDist  << "\n";
-                    posDist = -1;
+
+        while (buffi < gc) {
+
+            //Rain-Karp search algorithm
+            if (xHash == subHash && argumen.X == substring) {
+
+                if (absi - absPosSub  <= argumen.N) {
+
+                    if (posDist >= 0) {
+                        std::cout << rowDist << " " << posDist << "\n";
+                        posDist = -1;
+                    }
+                    absPosSub = absi;
+                    std::cout << row << " " << i  << "\n";
                 }
-                absPosSub = absi;
-                std::cout << row << " " << i  << "\n";
-            }
-            else {
+                else {
 
-                absPosSub = absi;
-                posDist = i;
-                rowDist = row;
+                    absPosSub = absi;
+                    posDist = i;
+                    rowDist = row;
 
-            }
+                }
             
-        }
+            }
 
-        // get new char
-        argumen.file.get(c);
-        if (int(c) < 0 || int(c) > 127)
-            return EXIT_FAILURE;
+            // get new char
+            //argumen.file.get(c);
+            c = buffer[buffi];
+            if (int(c) < 0 || int(c) > 127)
+                return EXIT_FAILURE;
 
-        //counting
-        i++;
-        absi++;
-        if (substring[0] == '\n') {
-            row++;
-            i = 0;
-        }
+            //counting
+            i++;
+            absi++;
+            buffi++;
+            if (substring[0] == '\n') {
+                row++;
+                i = 0;
+            }
 
-        // shift substring by one char
-         first = substring[0];
+            // shift substring by one char
+             first = substring[0];
         
-        for (buffi = 0; buffi < substring.size() - 1; buffi++) {
-            substring[buffi] = substring[buffi + 1];
-        }
-        substring[buffi] = c;
+            for (shifti = 0; shifti < substring.size() - 1; shifti++) {
+                substring[shifti] = substring[shifti + 1];
+            }
+            substring[shifti] = c;
 
-        subHash -= first;
-        subHash += substring[substring.size() - 1];
+            subHash -= first;
+            subHash += substring[substring.size() - 1];
+
+        }
+        
+        argumen.file.read(buffer, bufferSize);
+        gc = argumen.file.gcount();
+        buffi = 0;
 
     }
-
+     
   
 	return 0;
 }
@@ -119,7 +138,7 @@ std::optional<args> getArgs(int argc, char* argv[]) {
     arguments.X = argv[2];
     if (arguments.X.size() >= 256)
         return {};
-    //arguments.X = "Abababaabaabdbaaacbbbcbcacaab\ncdaadadcdcabbdbddadcadddcacaacbaabaabddacbcaddccaccddbdcdcc\ncbbddaddacabadccdccbbacabdccdccdbdbadacccbaacddcddadaacdabbaaddabdcabbcbdadadb\nabddaccdccccdddabbacdabcacdbcdabcbbcbb\nbcabbbcbbadbaaabdcbbaddcdacdadacccdddbaaacdd"; //; TMP hardcoded 
+    //arguments.X = "dccbbdcddddaabaad\nadd"; //; TMP hardcoded 
 
     int a;
     try {
